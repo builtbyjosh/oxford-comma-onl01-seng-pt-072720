9bc9dd5cf82fe3cb0f00a663df8a56a2575e7f5f
Joshua Collins <pc.refuge@gmail.com>
2020-07-31 03:50:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
-    if array.length == 1
+  if array.length == 2
+      array.join(" and ")
+  elsif array.length > 2
+      array.insert(-2, "and")
+      array[0..-1].join(", ")
+  else
       array.join
-    end
-
+  end
 end
